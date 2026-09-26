52db85f9eccc837dde4e8e289af9a79778b0f261
ljixin415-moker <ljixin415@gmail.com>
2026-09-26 08:11:20 +0800

    deploy: 2026-09-26

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // service worker: 导航/数据网络优先(每日新鲜)，静态资源缓存优先
-const CACHE = 'migration-intel-2026-09-25-0821';
+const CACHE = 'migration-intel-2026-09-26-0805';
 const CORE = ['./', './index.html', './manifest.json', './icons/icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
